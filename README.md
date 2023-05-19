89d4d77fb954e873f33e5dda4fd460ad70f14d3c
zeynab seyedi <seyede_zeinab_seyedi@yahoo.com>
2023-05-19 13:00:52 +0330

    styling

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Crypto
+![project screenshot](image link)
 This is a simple react project to display a list of digital currencies and their price changes.
 This project is part of my react learning path.
 
--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -2,7 +2,7 @@
 function Coin(props) {
     const {image,name,symbol,price,market_cap,price_change_percentage_24h}=props;
   return (
-    <div className="flex items-center gap-12 justify-around  border-b-2 ">
+    <div className="flex items-center gap-12 justify-around px-6 py-4  ">
         <img className="w-12  m-2" src={image} alt="name"/>
         <span className="w-28 p-4 text-sm font-bold">{name}</span>
         <span className="w-12 p-4 text-slate-700">{symbol.toUpperCase()}</span>
